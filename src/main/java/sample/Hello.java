8a83d52ac4c7cf41e32a306dd6b99e912301bab8
phanivallur <33691241+phanivallur@users.noreply.github.com>
2019-02-13 19:31:11 +0530

    commit 1

---
+++ src/main/java/sample/Hello.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Sujatha...  I'm glad that it finally worked!");
+	}
+
+}
